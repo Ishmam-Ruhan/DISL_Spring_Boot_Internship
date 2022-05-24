6d8e6221e377072703d204c8968cfb20f3d378ab
Ishmam Ruhan <ishmamruhan@gmail.com>
2022-05-24 11:27:07 +0600

    Solved: Java Stack - HackerRank

---
+++ Java_Stack.java
@@ -0,0 +1,86 @@
+import java.util.*;
+
+class Solution{
+    
+    /**
+    *
+    *   Problem Link: https://www.hackerrank.com/challenges/java-stack/problem?isFullScreen=true
+    *
+    *   Problem Name: Java Stack
+    *
+    */
+	
+	public static void main(String []argh)
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		while (sc.hasNext()) {
+			String input=sc.next();
+            
+            System.out.println(checkValidity(input));
+		}
+		
+	}
+    
+    
+    static boolean checkValidity(String pattern){
+        
+        Stack<Integer> characterValueStack = new Stack<Integer>();
+        
+        for(int i = 0; i < pattern.length(); i++){
+            
+            char character = pattern.charAt(i);
+            
+            if(getCharacterValue(character) >= 21 && 
+                    getCharacterValue(character) <=23){
+                        
+                int temporaryValue = 0;
+                
+                if(!characterValueStack.isEmpty())temporaryValue = characterValueStack.pop();
+                
+                if(temporaryValue != getCharacterValue(character) - 10)return false;
+                        
+            }else{
+                characterValueStack.push(getCharacterValue(character));
+            }
+            
+        }
+        
+        if(!characterValueStack.isEmpty())return false;
+        
+        return true;
+        
+    }
+    
+    
+    static int getCharacterValue(char character){
+        
+        switch(character){ 
+            case '(':
+                return 11;
+            
+            case '{':
+                return 12;
+            
+            case '[':
+                return 13;
+                
+            case ')':
+                return 21;
+            
+            case '}':
+                return 22;
+            
+            case ']':
+                return 23;
+            
+            default:
+                return -1;      
+        }
+        
+    }
+}
+
+
+
+
