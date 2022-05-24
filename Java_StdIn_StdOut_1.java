ac5c19206bf8426025f845c344aed25d72c1f012
Ishmam Ruhan <ishmamruhan@gmail.com>
2022-05-24 15:34:26 +0600

    Solved: Java If Else-Hackerrank

--- Java_StdIn_StdOut_1.java
+++ Java_StdIn_StdOut_1.java
@@ -2,6 +2,16 @@ import java.util.*;
 
 public class Solution {
 
+/*
+*
+*	Problem Link: https://www.hackerrank.com/challenges/java-stdin-and-stdout-1/problem?isFullScreen=true
+
+	Problem name: Java StdIn and Std Out
+	
+	Status: Solved
+*
+*/
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int a = scan.nextInt();
