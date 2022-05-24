de0950b9edb7c8f058d432754015af2f3a96c46b
Ishmam Ruhan <ishmamruhan@gmail.com>
2022-05-24 16:12:40 +0600

    Solved: java Output Formating-HackerRank

---
+++ JavaOutputFormatting.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("================================");
+        for(int i=0;i<3;i++)
+        {
+            String s1=sc.next();
+            int x=sc.nextInt();
+            String s2 = String.valueOf(x);
+
+            if(x < 100){
+
+                int len = s2.length();
+
+                for(int  j = 1; j <= (3 - len); j++){
+                    s2 = "0"+s2;
+                }
+
+            }
+
+            System.out.print(s1);
+
+            for(int  j = 0; j < (15 - s1.length()); j++){
+                System.out.print(" ");
+            }
+
+
+            System.out.println(s2);
+        }
+        System.out.println("================================");
+
+    }
+}
