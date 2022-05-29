8572f76908a0bf0a38da6876298e094c75139492
Ishmam Ruhan <ishmamruhan@gmail.com>
2022-05-29 13:20:21 +0600

    Solved: Java_Abstract_Class-HacckerRank

---
+++ Java_Abstract_Class.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+/*
+*	Problem Link: https://www.hackerrank.com/challenges/java-abstract-class/problem?isFullScreen=true
+*	Problem name: Java Abstract Class
+*	Status: Solved
+*
+*/
+
+
+
+
+abstract class Book{
+	String title;
+	abstract void setTitle(String s);
+	String getTitle(){
+		return title;
+	}
+	
+}
+
+class MyBook extends Book{
+    
+    @Override
+    void setTitle(String s) {
+        super.title = s;
+    }
+}
+
+public class Main{
+	
+	public static void main(String []args){
+		//Book new_novel=new Book(); This line prHMain.java:25: error: Book is abstract; cannot be instantiated
+		Scanner sc=new Scanner(System.in);
+		String title=sc.nextLine();
+		MyBook new_novel=new MyBook();
+		new_novel.setTitle(title);
+		System.out.println("The title is: "+new_novel.getTitle());
+      		sc.close();
+		
+	}
+}
